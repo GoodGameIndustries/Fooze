007140346b2cb79eebc1a48e3faa3cd0ba8b2a6e
EmmettGGI <goodgameindustries@gmail.com>
2015-05-27 22:38:34 -0400

    GDX setup
    General libgdx project setup

---
+++ desktop/src/com/GGI/Fooze/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.GGI.Fooze.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.GGI.Fooze.Fooze;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fooze(), config);
+	}
+}
